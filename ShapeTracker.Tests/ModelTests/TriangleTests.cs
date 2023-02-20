3b63f945b5b1716952b967ef0158fdc4a834fc95
Mitchell Gantz <mgantz22@icloud.com>
2023-02-20 11:42:31 -0800

    add passing test, code for Triangle.Side1 SET

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,6 +6,26 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
+
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle(2);
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+    
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      // Arrange
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      // Act
+      int result = newTriangle.Side1;
+      // Assert
+      Assert.AreEqual(length1, result);
+    }
+
     [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
     {
