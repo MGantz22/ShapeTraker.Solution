89c446e7c7ddf5f964134fcb67eedf761deecc3a
Mitchell Gantz <mgantz22@icloud.com>
2023-02-20 12:01:29 -0800

    add Side2 get fucnctionality + passing test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,20 +6,19 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
-
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle(2);
+      Triangle newTriangle = new Triangle(2, 3);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
-    
+
     [TestMethod]
     public void GetSide1_ReturnsSide1_Int()
     {
       // Arrange
       int length1 = 3;
-      Triangle newTriangle = new Triangle(length1);
+      Triangle newTriangle = new Triangle(length1, 2);
       // Act
       int result = newTriangle.Side1;
       // Assert
@@ -30,12 +29,23 @@ namespace ShapeTracker.Tests
     public void SetSide1_SetsValueOfSide1_Void()
     {
       // Arrange
-      Triangle newTriangle = new Triangle(3);
+      Triangle newTriangle = new Triangle(3,3);
       int newLength1 = 44;
       // Act
       newTriangle.Side1 = newLength1;
       // Assert
       Assert.AreEqual(newLength1, newTriangle.Side1);
+    }
+    [TestMethod]
+    public void GetSide2_ReturnsSide2_Int()
+    {
+      // Arrange
+      int length2 = 3;
+      Triangle newTriangle = new Triangle(2, length2);
+      // Act
+      int result = newTriangle.Side2;
+      // Assert
+      Assert.AreEqual(length2, result);
     }
 
   }
